a01d4d16263d890d179b4594c5a9642aadf57da6
rNexeR <rnexer@gmail.com>
2016-11-02 11:29:38 -0600

    Uploading project

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject clojure-rest "0.1.0-SNAPSHOT"
+  :description "Clojure Rest-Api"
+  :url "http://github.com/rNexeR"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.1.2"]
+                 [c3p0/c3p0 "0.9.1.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.168"]
+                 [cheshire "4.0.3"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler clojure-rest.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
